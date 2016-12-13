15c72f7eb8cce06dd6068d56f4dda80643f80074
foriin <liartom@mail.ru>
2016-12-13 14:39:53 +0300

    ctrl-c ctrl-ved evrything from module 2 to module 1 bc I was still on module 1

--- module1.R
+++ module1.R
@@ -131,3 +131,70 @@ get_p_value = function(test_data){
   sapply(test_data, '[', 2)}
 
 sapply(normality_tests, '[', 2)
+
+###############################
+#                             #
+#      DPLYR                  #
+#                             #
+###############################
+library("dplyr")
+library("ggplot2")
+diamonds <- diamonds
+
+slice(diamonds, c(1,4,5))
+
+filter(diamonds, carat > 0.3, color == 'J')
+subset(diamonds, carat > 0.3 & color == "J")
+arrange(diamonds, desc(price))
+arrange(diamonds, -price)
+mutate(diamonds, sqrt_price = price**2)
+nrow(diamonds) - !(nrow(diamonds)%%2)
+1 + !(344 %% 2)
+
+slice(diamonds, seq(1, nrow(diamonds) - !(nrow(diamonds) %% 2), by = 2))
+
+
+my_df <- select(mtcars, mpg, am, vs, hp) %>% 
+  filter(mpg > 14, hp > 100) %>% 
+  arrange(-mpg) %>% 
+  slice(1:10) %>% 
+  rename(`Miles per gallon` = mpg, `Gross horsepower` = hp)
+
+# Mutate each
+
+df <- as_data_frame(matrix(rnorm(40), ncol = 5))
+df %>% mutate_each(funs(. ** 2))
+all_to_factor <- function(x){
+  mutate_each(x, funs(as.factor(.)))
+  
+  
+}
+
+center <- function(x) (x - min(x))/(max(x) - min(x)) + 1
+center(rnorm(10))
+df$lol <- 'lol'
+
+df[,sapply(df, is.numeric)] <-  mutate_each(df[,sapply(df, is.numeric)], funs(log2(center(.))))
+
+# функция перевода заданных колонок в факторы
+
+to_factors <- function(df, factors){
+  df %>% mutate_at(factors, funs(as.factor(as.numeric(. > mean(.)))))
+}
+
+to_factors(mtcars[1:4], factors = c(1, 3))
+
+# High price
+
+high_price <- diamonds %>% 
+  select(color, price) %>% 
+  group_by(color) %>% 
+  arrange(-price) %>% 
+  slice(1:10)
+
+#########################
+#                       #
+#     DATA.TABLE        #
+#                       #
+#########################
+
